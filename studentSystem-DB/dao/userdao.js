14f166e15dfe5a5981ab5f051c99d49b539545c5
liqing <572298346@qq.com>
2021-01-25 10:28:21 +0800

    数据库的 文件夹

--- studentSystem-DB/dao/userdao.js
+++ studentSystem-DB/dao/userdao.js
@@ -1,18 +1,11 @@
-const userModel = require("../dao/model/userModel") 
-async function findUser(data) { 
+const userModel = require("../dao/model/userModel")
+async function findUser(data) {
     const users = await userModel.find(data)
     // const boo = users.some((item)=>item.username == data.username && item.password == data.password)
     return users;
 }
 
-function addUser(data) {
-    const boo = users.some((item)=>{
-        item.username == data.username && item.password == data.password
-    })
-    return boo;
-}
 
 module.exports = {
     findUser,
-    addUser
 }
